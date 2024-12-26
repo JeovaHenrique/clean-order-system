cca229b7b93b593e3c5a92d942971457f931859a
Jeova <jeovahenrique92@gmail.com>
2024-12-26 18:23:23 -0300

    Criacao de microserviço de autenticacao

---
+++ autenticacao/src/test/java/com/cleanordersystem/authentication/AuthenticationApplicationTest.java
@@ -0,0 +1,16 @@
+package com.cleanordersystem.authentication;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AuthenticationApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
